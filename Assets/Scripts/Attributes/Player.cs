352ce0440b6415649c42c29f5bbd96d47656d860
KristianHedeholm <kristian.hedeholm@gmail.com>
2022-08-21 21:59:07 +0200

    Added an attribute example with an undocumented attribute.

---
+++ Assets/Scripts/Attributes/Player.cs
@@ -0,0 +1,13 @@
+namespace Examples.Attributes.A1
+{
+    using UnityEngine;
+
+    // This Attribute is undocumented from Unity, but can be used. 
+    // You can read more about it here: https://forum.unity.com/threads/undocumented-but-public-defaultexecutionorder-attribute.530618/
+
+    [DefaultExecutionOrder(-200)]
+    public class Player : MonoBehaviour
+    {
+
+    }
+}
