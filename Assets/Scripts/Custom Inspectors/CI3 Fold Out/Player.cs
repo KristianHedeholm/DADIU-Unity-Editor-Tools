73d4b024113d2ad4d7722d2b10c2d588089c2e9f
KristianHedeholm <kristian.hedeholm@gmail.com>
2022-08-22 19:07:43 +0200

    Added 3. example for Custom Inspectors

---
+++ Assets/Scripts/Custom Inspectors/CI3 Fold Out/Player.cs
@@ -0,0 +1,13 @@
+namespace Examples.CustomInspectors.CI3
+{
+    using UnityEngine;
+
+    public class Player : MonoBehaviour
+    {
+        public int Health;
+        [SerializeField]
+        private int armo;
+
+        private int numberOfGuns;
+    }
+}
---
+++ Assets/Scripts/Custom Inspectors/CI3 Fold Out/PlayerCustomInspector.cs
@@ -0,0 +1,52 @@
+namespace Examples.CustomInspectors.CI3
+{
+    using UnityEditor;
+
+    [CustomEditor(typeof(Player))]
+    public class PlayerCustomInspector : Editor
+    {
+        private SerializedProperty Health;
+        private SerializedProperty armo;
+
+        private SerializedProperty numberOfGuns;
+
+        private bool showHealth;
+        private bool showArmo;
+
+        private void OnEnable()
+        {
+            Health = serializedObject.FindProperty("Health");
+            armo = serializedObject.FindProperty("armo");
+
+            numberOfGuns = serializedObject.FindProperty("numberOfGuns"); // this will not work since the field is not Serialized in the Player class!
+        }
+
+        public override void OnInspectorGUI()
+        {
+            showHealth = EditorGUILayout.Foldout(showHealth, "Show Health");
+            if(showHealth)
+            {
+                EditorGUILayout.IntField("Health", Health.intValue);
+            }
+
+            showArmo = EditorGUILayout.Foldout(showArmo, "Show Armo");
+            if(showArmo)
+            {
+                EditorGUILayout.PropertyField(armo);
+            }
+
+            // this will not work since the field is not Serialized in the Player class!
+            EditorGUILayout.PropertyField(numberOfGuns);
+        }
+    }
+
+    /*
+     * Links:
+     * SerializedObject: https://docs.unity3d.com/ScriptReference/SerializedObject.html
+     * SerializedProperty: https://docs.unity3d.com/ScriptReference/SerializedProperty.html
+     * SerializedObject.FindProperty: https://docs.unity3d.com/ScriptReference/SerializedObject.FindProperty.html
+     * EditorGUILayout.Foldout: https://docs.unity3d.com/ScriptReference/EditorGUILayout.Foldout.html
+     * EditorGUILayout.IntField: https://docs.unity3d.com/ScriptReference/EditorGUILayout.IntField.html
+     * EditorGUILayout.PropertyField: https://docs.unity3d.com/ScriptReference/EditorGUILayout.PropertyField.html
+     */
+}
