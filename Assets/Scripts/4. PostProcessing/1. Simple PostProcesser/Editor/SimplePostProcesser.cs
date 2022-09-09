dd912637963140cba87bba30a8a89d95d2e1dec2
KristianHedeholm <kristian.hedeholm@gmail.com>
2022-09-10 01:23:35 +0200

    Added Simple example for a pre and post processer.

---
+++ Assets/Scripts/4. PostProcessing/1. Simple PostProcesser/Editor/SimplePostProcesser.cs
@@ -0,0 +1,42 @@
+namespace Examples.Postprocessing.PP1
+{
+    using UnityEngine;
+    using UnityEditor;
+
+    public class SimplePostProcesser : AssetPostprocessor
+    {
+        void OnPreprocessAsset()
+        {
+            Debug.Log("Called Before Processing an asset");
+        }
+
+
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
+        {
+            Debug.Log("Called After Processing an asset");
+
+            foreach (var importedAsst in importedAssets)
+            {
+                foreach (string importAsset in importedAssets)
+                {
+                    Debug.Log("Reimported Asset: " + importAsset);
+                }
+
+                foreach (string deletedAsset in deletedAssets)
+                {
+                    Debug.Log("Deleted Asset: " + deletedAsset);
+                }
+
+                for (int i = 0; i < movedAssets.Length; i++)
+                {
+                    Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
+                }
+
+                if (didDomainReload)
+                {
+                    Debug.Log("Domain has been reloaded");
+                }
+            }
+        }
+    }
+}
