ba94413a2db46ad1aa025cee139f957d7a964b57
KristianHedeholm <kristian.hedeholm@gmail.com>
2022-09-11 16:26:29 +0200

    Removed Links to unsused APIs

--- Assets/Scripts/4. PostProcessing/2. Script Naming Checker/Editor/ScriptChecker.cs
+++ Assets/Scripts/4. PostProcessing/2. Script Naming Checker/Editor/ScriptChecker.cs
@@ -45,9 +45,6 @@ namespace Examples.Postprocessing.PP2
     /*
      * Links:
      * Path.GetExtension: https://docs.microsoft.com/en-us/dotnet/api/system.io.path.getextension?view=net-6.0
-     * MonoImporter: https://docs.unity3d.com/ScriptReference/MonoImporter.html
-     * 
-     * AssetImporter.GetAtPath: https://docs.unity3d.com/ScriptReference/AssetImporter.GetAtPath.html
      * File.ReadAllText: https://docs.microsoft.com/en-us/dotnet/api/system.io.file.readalltext?view=net-6.0
      * 
      * File.WriteAllText: https://docs.microsoft.com/en-us/dotnet/api/system.io.file.writealltext?view=net-6.0
