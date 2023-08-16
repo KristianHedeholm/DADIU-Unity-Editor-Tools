eedd09666a400c7cbeb094b415158f8aca9dd888
KristianHedeholm <kristian.hedeholm@gmail.com>
2023-08-16 23:08:18 +0200

    Added so we can load the Item database from a csv file.

--- Assets/Example Games/Scriptable Objects/Fantasy Shop Game/Scripts/ItemDatabase.cs
+++ Assets/Example Games/Scriptable Objects/Fantasy Shop Game/Scripts/ItemDatabase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEditor;
@@ -8,6 +9,83 @@ using UnityEditor;
 public class ItemDatabase : ScriptableObject
 {
     public ItemData[] _items;
+
+    [ContextMenu("Load CSV File")]
+    private void LoadDataBaseFromCSVFile()
+    {
+        _items = new ItemData[0];
+
+        var guids = AssetDatabase.FindAssets($"t:{typeof(TextAsset).Name}, items, ", new string[] { "Assets/" });
+        var path = AssetDatabase.GUIDToAssetPath(guids[0]);
+        path = path.Replace("Assets", "");
+        var fullPath = Path.GetFullPath(Application.dataPath + path);
+
+        var listOfItems = new List<ItemData>();
+
+        using (StreamReader reader = new StreamReader(fullPath))
+        {
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                var values = line.Split(',');
+
+                if (values[0].Contains("Name"))
+                {
+                    continue;
+                }
+
+                ItemData itemData = new ItemData();
+                itemData.Name = values[0];
+                itemData.ID = Convert.ToInt32(values[1]);
+                Debug.Log(itemData.ID);
+
+                var addedIndex = 2;
+                var tags = values[addedIndex];
+                if (!string.IsNullOrEmpty(tags))
+                {
+                    var endOfTagString = false;
+                    while (!endOfTagString)
+                    {
+                        tags += values[addedIndex];
+                        if (tags.EndsWith('"'))
+                        {
+                            endOfTagString = true;
+                            break;
+                        }
+                        addedIndex++;
+                    }
+                }
+                itemData.Tags = tags;
+                itemData.Price = Convert.ToInt32(values[addedIndex + 1]);
+                itemData.WeaponType = Enum.Parse<WeaponType>(values[addedIndex + 2]);
+
+
+                Stats stats = new Stats()
+                {
+                    Attack = Convert.ToInt32(values[addedIndex + 3]),
+                    Defence = Convert.ToInt32(values[addedIndex + 4]),
+                    Speed = Convert.ToInt32(values[addedIndex + 5]),
+                    Magic = Convert.ToInt32(values[addedIndex + 6]),
+                };
+
+                Reward reward = new Reward()
+                {
+                    Coins = Convert.ToInt32(values[addedIndex + 7]),
+                    IDsForItem = values[addedIndex + 8]
+                };
+
+                itemData.Stats = stats;
+                itemData.Reward = reward;
+                listOfItems.Add(itemData);
+            }
+        }
+        _items = listOfItems.ToArray();
+    }
 }
 
 [Serializable]
