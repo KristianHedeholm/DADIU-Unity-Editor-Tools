39304b062de272e4aa7af7c58ce84a4f787b9fa9
KristianHedeholm <kristian.hedeholm@gmail.com>
2023-08-17 23:09:28 +0200

    Added so we can get the serialized property for theitems.

--- Assets/Example Games/Scriptable Objects/Fantasy Shop Game/Scripts/Editor/ItemDatabaseCustomInspector.cs
+++ Assets/Example Games/Scriptable Objects/Fantasy Shop Game/Scripts/Editor/ItemDatabaseCustomInspector.cs
@@ -6,6 +6,13 @@ public class ItemDatabaseCustomInspector : Editor
 {
     private bool _useDefaultInspector = true;
 
+    private SerializedProperty _items;
+
+    private void OnEnable()
+    {
+        _items = serializedObject.FindProperty("_items");
+    }
+
     public override void OnInspectorGUI()
     {
         _useDefaultInspector = EditorGUILayout.Toggle("Use Default Inspector", _useDefaultInspector);
@@ -15,5 +22,7 @@ public class ItemDatabaseCustomInspector : Editor
             DrawDefaultInspector();
             return;
         }
+
+        EditorGUILayout.PropertyField(_items);
     }
 }
