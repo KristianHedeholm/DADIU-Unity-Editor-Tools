7c9e6c135e4b99f838ceecc22f3402bbcf15ece3
KristianHedeholm <kristian.hedeholm@gmail.com>
2023-08-25 19:53:11 +0200

    Worked some more on the Shop UI

--- Assets/Example Games/Scriptable Objects/Fantasy Shop Game/Scripts/ShopUI.cs
+++ Assets/Example Games/Scriptable Objects/Fantasy Shop Game/Scripts/ShopUI.cs
@@ -5,9 +5,17 @@ using UnityEngine.UI;
 
 public class ShopUI : MonoBehaviour
 {
+    public static ShopUI Instance;
+
     [SerializeField]
     private Text _shopName;
 
     [SerializeField]
     private ShopUIItem[] _shopUIItems;
+
+    private void Awake()
+    {
+        Instance = this;
+        
+    }
 }
